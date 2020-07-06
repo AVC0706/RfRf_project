dd69d09ce7b836d0edcf87822520b1aed8ef8d80
AVC0706 <atharvvijay4@gmail.com>
2020-07-06 20:10:24 +0530

    First commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoAtlas Connected");
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const connectDB = require("./config/database");
+const cors = require("cors");
+const app = express();
+
+//Database Connection
+connectDB();
+
+//MiddleWare
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+// app.use("/", (req, res) => {
+//   res.json({ msg: " This is node server" });
+// });
+
+
+
+//Defined Routes
+
+//----------Authentication--------------
+// app.use("/api/authenticate", require(""));
+
+
+
+
+// Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server Started on 5000"));
