f038959592402d4613c910cd0fd40d85978d97e1
kapushion <utkarshatre399@gmail.com>
2020-08-19 19:53:07 +0530

    meeting put and post

---
+++ models/meeting.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const MeetingSchema = mongoose.Schema({
+    mandal_id: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    date: {
+        type: Date
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    mom: {
+        type: String
+    },
+    agenda: {
+        type: String
+    },
+    mom_tags: [String],
+    pdf_link: {
+        type: String
+    }
+})
+
+const Meeting = mongoose.model("meeting",MeetingSchema);
+module.exports = Meeting;
