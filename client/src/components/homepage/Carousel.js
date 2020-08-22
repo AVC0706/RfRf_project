f15ed78f823736f66bf72e0a4520c7d15859ddd1
Rohit Pardeshi <rohitpardeshi04@users.noreply.github.com>
2020-08-22 19:51:41 +0530

    slideshow images+ contents added

---
+++ client/src/components/homepage/Carousel.js
@@ -0,0 +1,27 @@
+import { Carousel } from "antd";
+
+const contentStyle = {
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
+
+ReactDOM.render(
+  <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>,
+  mountNode
+);
--- routes/meeting.js
+++ routes/meeting.js
@@ -1,75 +1,69 @@
 const Meeting = require("../models/meeting");
-const Member = require("../models/member");
+// const Member = require("../models/member");
 const Mandal = require("../models/mandal");
-const {isAuth, isAdmin} = require("../middleware/auth");
+const { isAuth, isAdmin } = require("../middleware/auth");
 
 const express = require("express");
 const router = express.Router();
 
-router.post("/createmeeting",isAdmin,async (req,res)=> {
-    const {agenda,mom_tags,mandal_id} = req.body
-    try {
-       const meeting = new Meeting({
-           agenda,
-           mom_tags,
-           mandal_id,
-           user_id: req.user._id
-       })
+router.post("/createmeeting", isAdmin, async (req, res) => {
+  const { agenda, mom_tags, mandal_id } = req.body;
+  try {
+    const meeting = new Meeting({
+      agenda,
+      mom_tags,
+      mandal_id,
+      user_id: req.user._id,
+    });
 
-       await meeting.save()
-        res.status(201).send({meeting});
-    } catch(e) {
-        res.status(500).send({msg: "Server error"})
-    }
+    await meeting.save();
+    res.status(201).send({ meeting });
+  } catch (e) {
+    res.status(500).send({ msg: "Server error" });
+  }
 });
 
-router.put("/updatemeeting/:id",isAdmin, async (req,res)=> {
-    console.log(req.body);
-    try {
-        const meeting = await Meeting.findById(req.params.id);
-        if (!meeting) {
-            return res.status(204).json({ msg: "No data found" })
-        } 
-
-        meeting.agenda = req.body.agenda;
-        meeting.mom_tags = req.body.mom_tags;
-        await meeting.save()
-        res.status(200).send({meeting})
-    } catch(e) {
-        res.status(500).send({msg: "Server error"})
+router.put("/updatemeeting/:id", isAdmin, async (req, res) => {
+  console.log(req.body);
+  try {
+    const meeting = await Meeting.findById(req.params.id);
+    if (!meeting) {
+      return res.status(204).json({ msg: "No data found" });
     }
+
+    meeting.agenda = req.body.agenda;
+    meeting.mom_tags = req.body.mom_tags;
+    await meeting.save();
+    res.status(200).send({ meeting });
+  } catch (e) {
+    res.status(500).send({ msg: "Server error" });
+  }
 });
 
 //get meeting
-router.get("/getmeeting", async(req,res)=> {
-    
-    try {
-        const meeting = await Meeting.findById(req.params.id);
-        res.status(200).send(meeting);
-    } catch (e) {
-        console.error(e.message);
-        res.status(500).json({msg :"Server Error"});
-    }
-
+router.get("/getmeeting", async (req, res) => {
+  try {
+    const meeting = await Meeting.findById(req.params.id);
+    res.status(200).send(meeting);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
 
 //delete meeting
-router.delete("/deletemeeting/:id", isAdmin, async(req,res)=> {
-
-    try {
-        const meeting = await Meeting.findOneAndDelete(req.params.id);
-        if (!meeting) {
-            return res.status(204).json({ msg: "No data found" })
-        }
-
-        res.status(200).send({ meeting , msg: "Deleted meeting data" });
-
-    } catch (e) {
-        console.error(e.message);
-        res.status(500).send("Server Error");
+router.delete("/deletemeeting/:id", isAdmin, async (req, res) => {
+  try {
+    const meeting = await Meeting.findOneAndDelete(req.params.id);
+    if (!meeting) {
+      return res.status(204).json({ msg: "No data found" });
     }
 
+    res.status(200).send({ meeting, msg: "Deleted meeting data" });
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send("Server Error");
+  }
 });
 
-
-module.exports = router
+module.exports = router;
