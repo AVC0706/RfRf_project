e52b8de927d7b8cc86c05254cafd3f239c5bf280
Rugz007 <dsweb007@gmail.com>
2020-08-30 11:55:23 +0530

    Simple commit

---
+++ client/src/views/Home/MyMandals.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+
+function Dashboard() {
+
+  return (
+    <Layout >
+    </Layout>
+  );
+}
+
+export default Dashboard;
