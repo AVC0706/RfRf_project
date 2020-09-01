15be089532f1f574cfcecd8b17c53b0a89d7c10c
Rugz007 <dsweb007@gmail.com>
2020-09-01 11:16:34 +0530

    Added MOM Tags in Add Meetings

--- client/src/components/forms/AddMeeting.js
+++ client/src/components/forms/AddMeeting.js
@@ -1,28 +1,32 @@
-import React,{useState} from 'react';
-import { Form, Input,Button } from "antd";
+import React, { useState } from 'react';
+import { Form, Input, Button ,Select} from "antd";
+const {Option} = Select;
 function AddMeeting(props) {
     const [meeting, setmeeting] = useState({
         name: "",
         agenda: "",
+        tags: []
     });
-    const { name, agenda } = meeting;
-    const onChange = (e) => { 
+    const { name, agenda, tags } = meeting;
+    const onChange = (e) => {
         setmeeting({ ...meeting, [e.target.name]: e.target.value });
     };
-    const onFinish = () =>
-    {
+    const onChangeMOMTags = (e) => {
+        setmeeting({ ...meeting, tags: e });
+    }
+    const onFinish = () => {
         props.addMeeting();
     }
     return (
         <div>
-            <Form name = "addMeeting" onFinish = {onFinish}>
+            <Form name="addMeeting" onFinish={onFinish}>
                 <Form.Item
                     name="name"
                     label="Meeting Name"
                     rules={[{
                         required: true, message: "Please input meeting name",
                     }]}
-                    >
+                >
                     <Input name="name_input" value={name} onChange={onChange}></Input>
                 </Form.Item>
                 <Form.Item
@@ -32,6 +36,18 @@ function AddMeeting(props) {
                         required: true, message: "Please input agenda",
                     }]}>
                     <Input name="agenda_input" value={agenda} onChange={onChange}></Input>
+                </Form.Item>
+                <Form.Item>
+                    <Form.Item label="MOM Tags">
+                        <Select
+                            mode="multiple"
+                            style={{ width: '100%' }}
+                            placeholder="Please select"
+                            onChange={onChangeMOMTags}
+                        >
+                            <Option key = "example">Put tag here</Option>
+                                                         </Select>
+                    </Form.Item>
                 </Form.Item>
                 <Button
                     type="primary"
