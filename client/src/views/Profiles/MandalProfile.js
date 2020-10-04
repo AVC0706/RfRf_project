94737ae6b9f4a0973cb70d3abf726d261a31de40
kapushion <utkarshatre399@gmail.com>
2020-10-04 09:41:29 +0530

    localhost changed

--- client/src/views/Profiles/MandalProfile.js
+++ client/src/views/Profiles/MandalProfile.js
@@ -79,7 +79,7 @@ function MandalProfile(props) {
     console.log(props);
     axios
       .get(
-        `http://13.232.76.242:5000/api/mandal/getMandal/${props.match.params.id}`
+        `http://localhost:5000/api/mandal/getMandal/${props.match.params.id}`
       )
       .then((res) => {
         if (res.status === 200) {
@@ -97,7 +97,7 @@ function MandalProfile(props) {
     console.log(props);
     axios
       .get(
-        `http://13.232.76.242:5000/api/mandal/getMembers/${props.match.params.id}`
+        `http://localhost:5000/api/mandal/getMembers/${props.match.params.id}`
       )
       .then((res) => {
         if (res.status === 200) {
@@ -111,7 +111,7 @@ function MandalProfile(props) {
   };
   const getMeetings = () => {
     axios
-      .get(`http://13.232.76.242:5000/api/meeting/getmeeting/${props.match.params.id}`)
+      .get(`http://localhost:5000/api/meeting/getmeeting/${props.match.params.id}`)
       .then((res) => {
         if (res.status === 200) {
           console.log(res.data);
@@ -142,7 +142,7 @@ function MandalProfile(props) {
     };
     axios
       .post(
-        "http://13.232.76.242:5000/api/mandalAdmin/addMember",
+        "http://localhost:5000/api/mandalAdmin/addMember",
         {
           user,
           mandal_id: props.match.params.id,
@@ -171,7 +171,7 @@ function MandalProfile(props) {
     };
 
     axios.post(
-      `http://13.232.76.242:5000/api/meeting/createmeeting/${props.match.params.id}`,
+      `http://localhost:5000/api/meeting/createmeeting/${props.match.params.id}`,
       meeting,
       config
     ).then(res => {
@@ -193,7 +193,7 @@ function MandalProfile(props) {
     setLoading(true);
 
     axios
-      .delete(`http://13.232.76.242:5000/api/meeting/deletemeeting/${id}`)
+      .delete(`http://localhost:5000/api/meeting/deletemeeting/${id}`)
       .then((res) => {
         if (res.status === 200) {
           getMeetings();
