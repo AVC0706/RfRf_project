5724beb9c22e1e14d6f37d37947bd72c1dd30c39
Rugz007 <dsweb007@gmail.com>
2021-01-11 20:08:50 +0530

    Meeting Archive Search Added

--- client/src/views/Home/MeetingArchive.js
+++ client/src/views/Home/MeetingArchive.js
@@ -1,9 +1,15 @@
-import React, {useState,useEffect} from 'react';
-import {Button, Card, Col, Input, Row, Space, Table} from 'antd';
-import {SearchOutlined} from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Col, Input, Row, Space, Table } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import axios from "axios";
 
 function MeetingArchive() {
+    const [state, setState] = useState(
+        {
+            filterTable: null,
+            searchText: "",
+        }
+    );
     const getarchive = () => {
         axios.get(process.env.REACT_APP_SERVER_URL + `/meeting/getAllMeetings`)
             .then((res) => {
@@ -11,12 +17,32 @@ function MeetingArchive() {
                     setarchive(res.data)
                 }
             }).catch(e => {
-            console.log(e.message);
-        })
+                console.log(e.message);
+            })
     }
     useEffect(() => {
         getarchive()
     }, [])
+    const search = (value) => {
+        setState({
+            ...state,
+            searchText: value,
+            filterTable: archive.filter((o) =>
+                Object.keys(o).some((k) =>
+                    String(o[k]).toLowerCase().includes(value.toLowerCase())
+                )
+            ),
+        });
+    };
+    const onChange = (e) => {
+        if (e.value === "") {
+            setState({
+                ...state, filterTable: null, //baseData: props.mandals
+            });
+        }
+        setState({ ...state, searchText: e.value });
+    };
+    const { filterTable, searchText } = state;
     const [archive, setarchive] = useState([])
     const columns = [
         {
@@ -43,13 +69,20 @@ function MeetingArchive() {
     return (
         <div>
             <Row>
-                <Col span={2}/>
+                <Col span={2} />
                 <Col span={20}>
-                    <Card title={<h1>Meeting Archive</h1>}>
-                        <Table columns={columns} dataSource={archive}/>
+                    <Card title={<h1>Meeting Archive</h1>} extra={<Input.Search
+                            placeholder="Search"
+                            value={searchText}
+                            onChange={onChange}
+                            enterButton
+                            onSearch={search}
+                        />}>
+                        
+                        <Table columns={columns} dataSource={filterTable == null ? archive : filterTable} />
                     </Card>
                 </Col>
-                <Col span={2}/>
+                <Col span={2} />
 
             </Row>
 
